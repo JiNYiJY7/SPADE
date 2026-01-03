f2db0add2415b369f44665bce24ae86b02e0f5ef
Irdina07 <irdinaissam@gmail.com>
2026-01-03 17:56:21 +0800

    Update test_priority.py

--- tests/test_priority.py
+++ tests/test_priority.py
@@ -4,10 +4,10 @@ from study_planner.utils.scoring import priority_score
 
 
 def test_priority_deadline_closer_higher():
-    now = datetime.fromisoformat("2026-01-02T10:00:00")
-    t1 = Task(id="1", title="Close", due=datetime.fromisoformat("2026-01-02T12:00:00"),
-              est_minutes=60, remaining_minutes=60, importance=3)
-    t2 = Task(id="2", title="Far", due=datetime.fromisoformat("2026-01-05T12:00:00"),
-              est_minutes=60, remaining_minutes=60, importance=3)
+    current_time = datetime.fromisoformat("2026-01-02T10:00:00")
+    task_close_deadline = Task(id="1", title="Close", due=datetime.fromisoformat("2026-01-02T12:00:00"),
+              estimated_minutes=60, remaining_minutes=60, importance=3)
+    task_far_deadline = Task(id="2", title="Far", due=datetime.fromisoformat("2026-01-05T12:00:00"),
+              estimated_minutes=60, remaining_minutes=60, importance=3)
 
-    assert priority_score(t1, now) > priority_score(t2, now)
+    assert priority_score(task_close_deadline, current_time) > priority_score(task_far_dealine, current_time)
