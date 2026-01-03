4d709ab2cfc4fab0de41782037d515f080ecb25e
Irdina07 <irdinaissam@gmail.com>
2026-01-03 18:57:02 +0800

    Update test_scheduler.py

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -7,7 +7,7 @@ def test_scheduler_sessions_within_slot():
     current_time = datetime.fromisoformat("2026-01-02T10:00:00")
     task_list = [
         Task(id="A", title="Task A", due=datetime.fromisoformat("2026-01-03T10:00:00"),
-             estimated_minutes=120, remaining_minutes=120, importance=5)
+             est_minutes=120, remaining_minutes=120, importance=5)
     ]
     time_slots = [
         TimeSlot(
@@ -16,8 +16,8 @@ def test_scheduler_sessions_within_slot():
         )
     ]
 
-    study_plan = plan_sessions(tasks_sorted=task_list, free_slots=time_slots, now=now, chunk_minutes=50, break_minutes=10)
-    for session in plan.sessions:
+    study_plan = plan_sessions(tasks_sorted=task_list, free_slots=time_slots, now=current_time, chunk_minutes=50, break_minutes=10)
+    for session in study_plan.sessions:
         assert time_slots[0].start <= session.start <= time_slots[0].end
         assert time_slots[0].start <= session.end <= time_slots[0].end
         assert session.end > session.start
